edd1c579f8dad1232c0c5f324d51056e81203c70
Ownpro18 <thirumalkarthiga2018@gmail.com>
2021-11-22 13:30:34 +0000

    function added

--- revstring str _checking capitalisation.py
+++ revstring str _checking capitalisation.py
@@ -2,10 +2,14 @@ str = input("Enter the string ")
 rev = str[::-1]
 # for i in str:
 #     rev = i+rev
-print(rev)
-if str.islower()==True or str.isupper()==True:
-    print(True)
-elif str[0].isupper()==True and str[1:].islower()==True:
-    print(True)
-else:
-    print(False)
+def str_test():
+    print(rev)
+    if str.islower()==True or str.isupper()==True:
+        return True
+    elif str[0].isupper()==True and str[1:].islower()==True:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(str_test())
